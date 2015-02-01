873041e4910a94c8d98a9f451e602781429707f5
MR.T <tams1993@gmail.com>
2015-02-01 15:12:58 +0700

    Udacity Course SunShine Update 3

--- app/src/main/java/com/example/tams1993/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/tams1993/sunshine/ForecastFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -34,8 +36,23 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.forecastfragment, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+
+
+
+
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         String[] ForecastArray = {
